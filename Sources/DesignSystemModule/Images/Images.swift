6fc9f021ddd3eaa5ee96e13cb8a9866e66cdc55b
Dahyanna <dahyanna.hernandezmanriquez@cencosud.cl>
2024-01-08 16:38:07 -0300

    Init Components_

--- Sources/DesignSystemModule/Images/Images.swift
+++ Sources/DesignSystemModule/Images/Images.swift
@@ -15,12 +15,17 @@ public struct ImageView: View {
     public init() {}
 
     public var body: some View {
-        Image(Images.ICON_HOME.rawValue)
-            
+        Image(Images.ICON_HOME, bundle: .module)
+            .resizable()
+            .scaledToFit()
+            .frame(height: 200)
     }
 }
+//#Preview {
+//    ImageView()
+//}
 
-public enum Images: String {
-    case LOGO_PARIS = "icon_cart"
-    case ICON_HOME = "icon_home"
+public enum Images {
+    static let LOGO_PARIS = "main_logo_paris"
+    static let ICON_HOME = "home_icon"
 }
